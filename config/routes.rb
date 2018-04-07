6ca114a874c7e2855db613ee5b6ac1e51aef4b0d
Julian <hello@juliantrueflynn.com>
2018-04-07 15:14:38 -0400

    Add nested resource for Channels

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:index, :show]
     resources :workspaces, only: [:index, :show, :create, :destroy] do
+      resources :channels, only: [:index, :show, :create, :destroy]
     end
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
