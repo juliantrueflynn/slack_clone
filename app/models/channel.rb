69cf33160e73776ea698ce9b0682e7b4fc3cc21d
Julian <hello@juliantrueflynn.com>
2018-04-07 15:17:09 -0400

    Add workspace_id to Channels model validations

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,5 +1,5 @@
 class Channel < ApplicationRecord
-  validates :title, :owner_id, presence: true
+  validates :title, :owner_id, :workspace_id, presence: true
 
   belongs_to :owner, foreign_key: :owner_id, class_name: 'User'
   belongs_to :workspace
