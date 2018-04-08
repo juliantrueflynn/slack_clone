9fdc922bb69f78763a6ea772947ba6aef06d221c
Julian <hello@juliantrueflynn.com>
2018-04-07 23:28:37 -0400

    Update Channels controller to be more based on user and workspace

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,14 +1,31 @@
 class Api::ChannelsController < ApplicationController
   def index
-    @channels = Channel.all
+    @channels = Channel.subbed_by_user_in_workspace(current_user.id, params[:workspace_id])
   end
 
   def show
+    @channel = Channel.find(params[:id])
   end
 
   def create
+    @channel = Channel.new(channel_params)
+
+    if @channel.save
+      render json: @channel
+    else
+      render json: @channel.errors.full_messages
+    end
   end
 
   def destroy
+    @channel = Channel.find(params[:id])
+    @channel.destroy
+    render json: @channel
+  end
+
+  private
+
+  def channel_params
+    params.require(:channel).permit(:title, :topic, :owner_id, :workspace_id)
   end
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -9,4 +9,8 @@ class Channel < ApplicationRecord
     users_subbed = subs.where(channel_subs: { user_id: user.id })
     users_subbed.length > 0
   end
+
+  def self.subbed_by_user_in_workspace(user_id, workspace_id)
+    self.joins(:subs).where(channel_subs: {user_id: user_id})
+  end
 end
