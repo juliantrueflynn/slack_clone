9cc2a2ad4b7bfaba6f95559dc3f4ee2fc80e4128
Julian <hello@juliantrueflynn.com>
2018-04-08 17:23:29 -0400

    Update webpack.config to production ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
 var path = require("path");
+var webpack = require("webpack");
 
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+// include plugins config
 module.exports = {
-  mode: 'development',
   context: __dirname,
-  entry: "./frontend/slack_clone.jsx",
+  entry: "./frontend/<name of entry file>",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     rules: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['react', 'es2015']
         }
       }
     ]
