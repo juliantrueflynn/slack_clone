1f957a4f4ecbc7a090b5f5c36a97aa4d34809406
Julian <hello@juliantrueflynn.com>
2018-04-09 01:41:55 -0400

    Update so displayWorkspaceId updates with Channel action

--- frontend/reducers/workspace_display_reducer.js
+++ frontend/reducers/workspace_display_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_WORKSPACE } from '../actions/workspace_actions';
+import { RECEIVE_CHANNEL } from '../actions/channel_actions';
 
 const workspaceDisplayReducer = (state = null, action) => {
   Object.freeze(state);
@@ -6,6 +7,8 @@ const workspaceDisplayReducer = (state = null, action) => {
   switch (action.type) {
     case RECEIVE_WORKSPACE :
       return action.workspace.workspace.id;
+    case RECEIVE_CHANNEL :
+      return action.channel.channel.workspaceId;
     default :
       return state;
   }
