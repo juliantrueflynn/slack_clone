354cd9799f263188b6646c6b2c8df4683ffff307
Julian <hello@juliantrueflynn.com>
2018-04-09 16:45:51 -0400

    Fix case receive Workspace constant wrong id

--- frontend/reducers/workspace_display_reducer.js
+++ frontend/reducers/workspace_display_reducer.js
@@ -6,7 +6,7 @@ const workspaceDisplayReducer = (state = null, action) => {
 
   switch (action.type) {
     case RECEIVE_WORKSPACE :
-      return action.workspace.workspace.id;
+      return action.workspace.id;
     case RECEIVE_CHANNEL :
       return action.channel.channel.workspaceId;
     default :
