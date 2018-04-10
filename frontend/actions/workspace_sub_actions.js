c7e33b45b520df762cbd25650c7bc5eb7514833c
Julian <hello@juliantrueflynn.com>
2018-04-10 16:34:42 -0400

    Update to have create_workspace_sub constant

--- frontend/actions/workspace_sub_actions.js
+++ frontend/actions/workspace_sub_actions.js
@@ -1,14 +1,20 @@
 import * as WorkspaceSubAPIUtil from '../util/workspace_sub_api_util';
 
 export const RECEIVE_WORKSPACE_SUB = 'RECEIVE_WORKSPACE_SUB';
+export const CREATE_WORKSPACE_SUB = 'CREATE_WORKSPACE_SUB';
 
 export const receiveWorkspaceSub = workspaceSub => ({
   type: RECEIVE_WORKSPACE_SUB,
   workspaceSub
 });
 
-export const createWorkspaceSub = workspaceSub => dispatch => (
+export const createWorkspaceSub = workspaceSub => ({
+  type: CREATE_WORKSPACE_SUB,
+  workspaceSub
+});
+
+export const createNewWorkspaceSub = workspaceSub => dispatch => (
   WorkspaceSubAPIUtil.createWorkspaceSub(workspaceSub).then(
-    newWorkspaceSub => dispatch(receiveWorkspaceSub(newWorkspaceSub))
+    newWorkspaceSub => dispatch(createWorkspaceSub(newWorkspaceSub))
   )
 );
