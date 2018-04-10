5a9c2a3d6765f9b688bdad7fcd2caf18f290c8f6
Julian <hello@juliantrueflynn.com>
2018-04-09 20:22:08 -0400

    Update on action receive_channel to populate matching object

--- frontend/reducers/channel_reducer.js
+++ frontend/reducers/channel_reducer.js
@@ -4,11 +4,19 @@ import { RECEIVE_WORKSPACE } from '../actions/workspace_actions';
 const channelReducer = (state = {}, action) => {
   Object.freeze(state);
 
+  let nextState;
   switch (action.type) {
     case RECEIVE_CHANNELS :
-      return action.channels;
+      return Object.assign({}, state, action.channels);
+    case RECEIVE_CHANNEL :
+      let channel = action.channel.channel;
+      channel.messageIds = action.channel.messages.map(msg => msg.id);
+      channel.memberIds = action.channel.members.map(member => member.id);
+      nextState = { [action.channel.channel.id]: channel };
+      return Object.assign({}, state, nextState);
     case RECEIVE_WORKSPACE :
-      return action.workspace.channels;
+      nextState = action.workspace.channels;
+      return Object.assign({}, state, nextState);
     default :
       return state;
   }
