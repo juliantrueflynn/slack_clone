0caf76c1ddde7071ebc3e8125df605396ae1f54b
Julian <hello@juliantrueflynn.com>
2018-04-09 20:33:08 -0400

    Refactor and further testing

--- frontend/reducers/workspace_reducer.js
+++ frontend/reducers/workspace_reducer.js
@@ -1,16 +1,21 @@
 import merge from 'lodash/merge';
-import { RECEIVE_WORKSPACES, RECEIVE_WORKSPACE } from '../actions/workspace_actions';
+import {
+  RECEIVE_WORKSPACES,
+  RECEIVE_WORKSPACE
+} from '../actions/workspace_actions';
 
 const workspaceReducer = (state = {}, action) => {
   Object.freeze(state);
 
+  let nextState;
   switch (action.type) {
     case RECEIVE_WORKSPACES :
-      return action.workspaces;
+      nextState = action.workspaces;
+      return Object.assign({}, state, nextState);
     case RECEIVE_WORKSPACE :
-      const workspace = action.workspace.workspace;
-      workspace.channelIds = action.workspace.channels.map(channel => channel.id);
-      const nextState = { [workspace.id]: workspace };
+      const { workspace, channels } = action.workspace;
+      workspace.channelIds = channels.map(channel => channel.id);
+      nextState = { [workspace.id]: workspace };
       return Object.assign({}, state, nextState);
     default :
       return state;
