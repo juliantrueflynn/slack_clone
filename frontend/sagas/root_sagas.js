6ae6b4643cbc8e3961e3094d59b909836aadeb9d
Julian <hello@juliantrueflynn.com>
2018-04-10 14:18:40 -0400

    Init root saga generator

---
+++ frontend/sagas/root_sagas.js
@@ -0,0 +1,10 @@
+import { call, putt, takeEvery, takeLatest } from 'redux-saga/effects';
+import {
+  RECEIVE_WORKSPACES,
+  RECEIVE_WORKSPACE
+} from '../actions/workspace_actions';
+
+function* rootSagas() {
+}
+
+export default rootSagas;
