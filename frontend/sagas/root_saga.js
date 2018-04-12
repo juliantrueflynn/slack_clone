139c83c803a325644d45cc138dc15ae708cfb1da
Julian <hello@juliantrueflynn.com>
2018-04-12 15:38:22 -0400

    Revert root_saga back to preloadedState version

--- frontend/sagas/root_saga.js
+++ frontend/sagas/root_saga.js
@@ -1,6 +1,11 @@
 import { fork, all } from 'redux-saga/effects';
 import { workspaceSaga } from './workspace_saga';
 
-export default function* root() {
-  yield all([fork(workspaceSaga)]);
-}
+// export default function* root() {
+//   yield all([fork(workspaceSaga)]);
+// }
+
+const sagas = [workspaceSaga];
+
+export const rootSaga = sagaMiddleware =>
+  sagas.forEach(sagaMiddleware.run.bind(sagaMiddleware));
