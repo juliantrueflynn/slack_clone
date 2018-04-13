49f7cab0b5c3cbe975bbaf7721863adb27f7b6a4
Julian <hello@juliantrueflynn.com>
2018-04-13 02:11:46 -0400

    Add channel saga

--- frontend/sagas/root_saga.js
+++ frontend/sagas/root_saga.js
@@ -1,6 +1,7 @@
 import { fork, all } from 'redux-saga/effects';
 import { workspaceSaga } from './workspace_saga';
+import { channelSaga } from './channel_saga';
 
 export default function* root() {
-  yield all([fork(workspaceSaga)]);
+  yield all([fork(workspaceSaga), fork(channelSaga)]);
 }
