f74b6082c6c74bcf8d31d6518875ff8dd4200251
Julian <hello@juliantrueflynn.com>
2018-04-14 12:50:09 -0400

    Remove final thunks

--- frontend/store/store.prod.js
+++ frontend/store/store.prod.js
@@ -1,6 +1,5 @@
 import "regenerator-runtime/runtime";
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 import createSagaMiddleware, { END } from 'redux-saga';
 import logger from 'redux-logger';
