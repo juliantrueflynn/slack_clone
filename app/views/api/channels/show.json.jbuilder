6ac61bb09d8c2160e25b5cb35c0b96fe13931c8d
Julian <hello@juliantrueflynn.com>
2018-04-14 20:03:19 -0400

    Replace jbuilder camelizing for humps lib

--- app/views/api/channels/show.json.jbuilder
+++ app/views/api/channels/show.json.jbuilder
@@ -1,8 +1,8 @@
 json.set! :channel do
   json.extract! @channel, :id, :title, :topic
-  json.ownerId @channel.owner_id
-  json.workspaceId @channel.workspace_id
-  json.isSubbed current_user.is_channel_sub?(@channel)
+  json.owner_id @channel.owner_id
+  json.workspace_id @channel.workspace_id
+  json.is_subbed current_user.is_channel_sub?(@channel)
 end
 
 json.set! :members do
@@ -17,8 +17,8 @@ json.set! :messages do
   json.array! @channel.messages do |message|
     json.id message.id
     json.body message.body
-    json.authorId message.author_id
-    json.channelId message.channel_id
-    json.parentMessageId message.parent_message_id
+    json.author_id message.author_id
+    json.channel_id message.channel_id
+    json.parent_message_id message.parent_message_id
   end
 end
--- frontend/util/channel_api_util.js
+++ frontend/util/channel_api_util.js
@@ -1,25 +1,25 @@
-import { camelizeKeys } from 'humps';
+import { camelizeKeys, decamelizeKeys } from 'humps';
 
-export const fetchChannels = workspaceId => (
-  fetch(`api/workspaces/${workspaceId}/channels`, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include'
-  }).then(response =>
-    response.json().then(json => ({ json, response }))
-  ).then(({ json, response }) => {
-    if (!response.ok) {
-      throw json;
-    }
+// export const fetchChannels = workspaceId => (
+//   fetch(`api/workspaces/${workspaceId}/channels`, {
+//     method: 'GET',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     credentials: 'include'
+//   }).then(response =>
+//     response.json().then(json => ({ json, response }))
+//   ).then(({ json, response }) => {
+//     if (!response.ok) {
+//       throw json;
+//     }
 
-    return camelizeKeys(json);
-  }).catch(error => {
-    throw error.message || ['Unknown channel error!'];
-  })
-);
+//     return camelizeKeys(json);
+//   }).catch(error => {
+//     throw error.message || ['Unknown channel error!'];
+//   })
+// );
 
 export const fetchChannel = channelId => (
   fetch(`api/channels/${channelId}`, {
@@ -49,7 +49,7 @@ export const createChannel = channel => (
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(channel)
+    body: JSON.stringify(decamelizeKeys(channel, { separator: '_' }))
   }).then(response =>
     response.json().then(json => ({ json, response }))
   ).then(({ json, response }) => {
