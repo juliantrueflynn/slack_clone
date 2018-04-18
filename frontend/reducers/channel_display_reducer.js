f3e186a1b271f29237252fa010875f013049615b
Julian <hello@juliantrueflynn.com>
2018-04-18 14:56:42 -0400

    Set null for channelId if user ends up on Workspace page somehow

--- frontend/reducers/channel_display_reducer.js
+++ frontend/reducers/channel_display_reducer.js
@@ -1,10 +1,13 @@
 import merge from 'lodash/merge';
 import { LOAD_CHANNEL_PAGE } from '../actions/channel_actions';
+import { LOAD_WORKSPACE_PAGE } from '../actions/workspace_actions';
 
 const channelDisplayReducer = (state = null, action) => {
   Object.freeze(state);
 
   switch (action.type) {
+    case LOAD_WORKSPACE_PAGE :
+      return null;
     case LOAD_CHANNEL_PAGE :
       return action.channelId;
     default :
