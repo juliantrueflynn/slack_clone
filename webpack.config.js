6a30ad542359bda28a3849d44701efd2efad3954
Julian <hello@juliantrueflynn.com>
2018-04-19 19:22:24 -0400

    Install css loaders for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
         query: {
           presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
