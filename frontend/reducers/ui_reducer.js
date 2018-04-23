3e357a1dd26eb303780f35283e588b17f2457405
Julian <hello@juliantrueflynn.com>
2018-04-22 23:09:54 -0400

    Init thread open/close actions and reducer

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -3,12 +3,14 @@ import displayWorkspaceId from './workspace_display_reducer';
 import displayChannelId from './channel_display_reducer';
 import displayModal from './modal_reducer';
 import editMessageId from './message_edit_reducer';
+import displayThreadId from './message_thread_reducer';
 
 const uiReducer = combineReducers({
   displayWorkspaceId,
   displayChannelId,
   displayModal,
   editMessageId,
+  displayThreadId,
 });
 
 export default uiReducer;
