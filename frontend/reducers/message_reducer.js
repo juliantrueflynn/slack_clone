6a95bf76e232fd5455a34e9214a3fbc17b30d123
Julian <hello@juliantrueflynn.com>
2018-04-25 20:34:53 -0400

    Update so threadIds property added to active thread

--- frontend/reducers/message_reducer.js
+++ frontend/reducers/message_reducer.js
@@ -2,6 +2,7 @@ import { RECEIVE_CHANNEL } from '../actions/channel_actions';
 import {
   CREATE_MESSAGE_SUCCESS, EDIT_MESSAGE_SUCCESS, DELETE_MESSAGE_SUCCESS
 } from '../actions/message_actions';
+import { OPEN_THREAD } from '../actions/message_thread_actions';
 
 const messageReducer = (state = {}, action) => {
   Object.freeze(state);
@@ -25,6 +26,16 @@ const messageReducer = (state = {}, action) => {
     case DELETE_MESSAGE_SUCCESS :
       nextState = Object.assign({}, state);
       delete nextState[action.messageId];
+      return nextState;
+    case OPEN_THREAD :
+      nextState = Object.assign({}, state);
+      const threadIds = [];
+      Object.values(state).map(message => {
+        if (message.parentMessageId === action.messageId) {
+          threadIds.push(message.id);
+        }
+      });
+      nextState[action.messageId].threadIds = threadIds;
       return nextState;
     default :
       return state;
