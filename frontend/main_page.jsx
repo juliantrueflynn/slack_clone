ea111c50849b906f03e5c30449b7ce86d4a3b879
Julian <hello@juliantrueflynn.com>
2018-05-08 15:10:37 -0400

    Move ActionCableProvider further up hierarchy to avoid interference

--- frontend/main_page.jsx
+++ frontend/main_page.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ActionCableProvider } from 'react-actioncable-provider';
 import { AuthRoute } from './util/route_util';
 import { Switch, Link, Route } from 'react-router-dom';
 import ChannelSessionFormContainer
@@ -13,33 +12,31 @@ import ChannelPageContainer
 import './main_page.css';
 
 const MainPage = () => (
-  <div className="main-page">
-    <ActionCableProvider url="ws://localhost:3000/cable">
-      <Switch>
-        <Route exact path="/" render={ () =>
-          <div className="content-container content-container__boxed-width">
-            <h1>Slack Clone with Rails & React</h1>
-            <p>More will go here later, stay-tuned!</p>
-            <Link to="/create-workspace">Create New Workspace</Link>
-          </div>
-        } />
-        <AuthRoute
-          path="/signin"
-          component={ ChannelSessionFormContainer } />
-        <AuthRoute
-          path="/signup"
-          component={ ChannelSessionFormContainer } />
-        <Route
-          path="/create-workspace"
-          component={ WorkspaceFormContainer } />
-        <Route
-          path="/:workspaceSlug/:channelSlug"
-          component={ ChannelPageContainer } />
-        <Route
-          path="/:workspaceSlug"
-          component={ WorkspacePageContainer } />
-      </Switch>
-    </ActionCableProvider>
+  <div className="main-page">    
+    <Switch>
+      <Route exact path="/" render={ () =>
+        <div className="content-container content-container__boxed-width">
+          <h1>Slack Clone with Rails & React</h1>
+          <p>More will go here later, stay-tuned!</p>
+          <Link to="/create-workspace">Create New Workspace</Link>
+        </div>
+      } />
+      <AuthRoute
+        path="/signin"
+        component={ ChannelSessionFormContainer } />
+      <AuthRoute
+        path="/signup"
+        component={ ChannelSessionFormContainer } />
+      <Route
+        path="/create-workspace"
+        component={ WorkspaceFormContainer } />
+      <Route
+        path="/:workspaceSlug/:channelSlug"
+        component={ ChannelPageContainer } />
+      <Route
+        path="/:workspaceSlug"
+        component={ WorkspacePageContainer } />
+    </Switch>
   </div>
 );
 
