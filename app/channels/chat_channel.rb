cada349f90144e4a22190b3b9d8a175bed634378
Julian <hello@juliantrueflynn.com>
2018-05-07 21:38:25 -0400

    Refactor so channel has create method, more DRY

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,26 +1,9 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'chat_channel'
+    stream_from "chat_channel"
   end
 
-  def unsubscribed; end
-
-  def create(options)
-    Message.create(
-      body: options.fetch('body'),
-      author_id: options.fetch('author_id'),
-      channel_id: options.fetch('channel_id'),
-      parent_message_id: options.fetch('parent_message_id')
-    )
-  end
-
-  def update(options)
-    message = Message.find_by(id: options.fetch('id'))
-    message.update(body: options.fetch('body'))
-  end
-
-  def delete(options)
-    message = Message.find_by(id: options.fetch('id'))
-    message.destroy
+  def unsubscribed
+    stop_all_streams
   end
 end
