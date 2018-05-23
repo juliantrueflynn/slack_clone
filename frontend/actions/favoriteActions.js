2fb027646c0ca55f6da24918ca2b7526ab8923aa
Julian <hello@juliantrueflynn.com>
2018-05-22 21:54:16 -0400

    Update with favorites index actions/util

--- frontend/actions/favoriteActions.js
+++ frontend/actions/favoriteActions.js
@@ -1,9 +1,26 @@
+export const FAVORITES_REQUEST = 'FAVORITES_REQUEST';
+export const FAVORITES_RECEIVE = 'FAVORITES_RECEIVE';
+export const FAVORITES_FAILURE = 'FAVORITES_FAILURE';
 export const CREATE_FAVORITE_REQUEST = 'CREATE_FAVORITE_REQUEST';
 export const CREATE_FAVORITE_RECEIVE = 'CREATE_FAVORITE_RECEIVE';
 export const CREATE_FAVORITE_FAILURE = 'CREATE_FAVORITE_FAILURE';
 export const DELETE_FAVORITE_REQUEST = 'DELETE_FAVORITE_REQUEST';
 export const DELETE_FAVORITE_RECEIVE = 'DELETE_FAVORITE_RECEIVE';
 export const DELETE_FAVORITE_FAILURE = 'DELETE_FAVORITE_FAILURE';
+
+export const favoritesRequest = () => ({
+  type: FAVORITES_REQUEST
+});
+
+export const favoritesReceive = favorites => ({
+  type: FAVORITES_RECEIVE,
+  favorites
+});
+
+export const favoritesFailure = errors => ({
+  type: FAVORITES_FAILURE,
+  errors
+});
 
 export const createFavoriteRequest = messageSlug => ({
   type: CREATE_FAVORITE_REQUEST,
--- frontend/util/favoriteAPIUtil.js
+++ frontend/util/favoriteAPIUtil.js
@@ -1,5 +1,25 @@
 import { decamelizeKeys } from 'humps';
 
+export const fetchFavorites = () => (
+  fetch('api/message_favs', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include'
+  }).then(response =>
+    response.json().then(json => ({ json, response }))
+  ).then(({ json, response }) => {
+    if (!response.ok) {
+      throw json;
+    }
+    return json;
+  }).catch(error => {
+    throw error || ['Unknown message error!'];
+  })
+);
+
 export const createFavorite = messageFav => (
   fetch('api/message_favs', {
     method: 'POST',
