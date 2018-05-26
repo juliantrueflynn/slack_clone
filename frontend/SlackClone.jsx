e54121a84d21264707674c411d1edfb541490ee3
Julian <hello@juliantrueflynn.com>
2018-05-26 13:06:13 -0400

    Remove test imports to try to fix Unknown component

--- frontend/SlackClone.jsx
+++ frontend/SlackClone.jsx
@@ -4,17 +4,6 @@ import configureStore from './store/store';
 import Root from './Root';
 import rootSaga from './sagas/rootSaga';
 
-// Just for testing, remove!
-import * as WorkspaceActions from './actions/workspaceActions';
-import * as WorkspaceAPIUtil from './util/workspaceAPIUtil';
-import * as WorkspaceSubActions from './actions/workspaceSubActions';
-import * as ChannelActions from './actions/channelActions';
-import * as ChannelAPIUtil from './util/channelAPIUtil';
-import * as MessageAPIUtil from './util/messageAPIUtil';
-import * as MessageActions from './actions/messageActions';
-import * as FavoriteAPIUtil from './util/favoriteAPIUtil';
-import * as FavoriteActions from './actions/favoriteActions';
-
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
 
@@ -28,21 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   store.runSaga(rootSaga);
-
-  // Just for testing, remove!
-  if (process.env.NODE_ENV !== 'production') {
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    window.WorkspaceActions = WorkspaceActions;
-    window.WorkspaceAPIUtil = WorkspaceAPIUtil;
-    window.WorkspaceSubActions = WorkspaceSubActions;
-    window.ChannelActions = ChannelActions;
-    window.ChannelAPIUtil = ChannelAPIUtil;
-    window.MessageActions = MessageActions;
-    window.MessageAPIUtil = MessageAPIUtil;
-    window.FavoriteActions = FavoriteActions;
-    window.FavoriteAPIUtil = FavoriteAPIUtil;
-  }
 
   ReactDOM.render(<Root store={store} />, rootEl);
 });
