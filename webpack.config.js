15c5e0cd653f5e8c6b93dbe7e0cd0d625239587f
Julian <hello@juliantrueflynn.com>
2018-05-26 14:24:43 -0400

    Test arrow functions babel to fix Unknown components

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path");
 var webpack = require("webpack");
 
-var plugins = []; // if using any plugins for both dev and production
+var plugins = ["transform-es2015-arrow-functions"]; // dev and prod plugins
 var devPlugins = []; // if using any plugins for development
 
 var prodPlugins = [
