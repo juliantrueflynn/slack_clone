362b65995b2a4e683cbe32972dfd4203e2cbf1ef
Julian <hello@juliantrueflynn.com>
2018-05-29 18:33:36 -0400

    Init emoji working for messages

--- frontend/components/Message/Message.jsx
+++ frontend/components/Message/Message.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import MessageEditForm from './MessageEditForm';
 import MessageHoverMenu from './MessageHoverMenu';
+import Editor from 'draft-js-plugins-editor';
+import { EditorState, convertFromRaw } from 'draft-js';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+import 'draft-js/dist/Draft.css';
+import 'draft-js-emoji-plugin/lib/plugin.css';
+
+const emojiPlugin = createEmojiPlugin();
+const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
 
 class Message extends React.Component {
   constructor(props) {
     super(props);
 
+    const body = JSON.parse(props.message.body);
+    const blocks = convertFromRaw(body);
     this.state = {
       isMouseOver: false,
-      isEditing: false
+      isEditing: false,
+      editorState: EditorState.createWithContent(blocks)
     };
 
+    this.onChange = editorState => this.setState({ editorState });
+  
     this.handleEditToggle = this.handleEditToggle.bind(this);
   }
 
@@ -58,7 +71,12 @@ class Message extends React.Component {
               ID: #{this.props.message.id}<br/>
               Slug: {this.props.message.slug}<br/>
               Author: {this.props.message.authorId}<br/>
-              Body: {this.props.message.body}
+              <Editor
+                editorState={this.state.editorState}
+                onChange={this.onChange}
+                plugins={[emojiPlugin]}
+                readOnly
+              />
             </div>
           )}
         </div>
--- frontend/components/Message/MessageForm.jsx
+++ frontend/components/Message/MessageForm.jsx
@@ -1,45 +1,62 @@
 import React from 'react';
+import Editor from 'draft-js-plugins-editor';
+import { EditorState, convertToRaw } from 'draft-js';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
 import FormErrors from '../Layout/FormErrors';
- 
+import 'draft-js/dist/Draft.css';
+import 'draft-js-emoji-plugin/lib/plugin.css';
+
+const emojiPlugin = createEmojiPlugin();
+const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
+
 class MessageForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { body: '' };
+    this.state = { editorState: EditorState.createEmpty() };
 
-    this.handleTextareaValue = this.handleTextareaValue.bind(this);
+    this.onChange = editorState => this.setState({
+      editorState
+    });
+
     this.handleMessageSubmit = this.handleMessageSubmit.bind(this);
-  }
-
-  handleTextareaValue(event) {
-    const body = event.currentTarget.value;
-    this.setState({ body });
   }
 
   handleMessageSubmit(event) {
     event.preventDefault();
 
+    const currentContent = this.state.editorState.getCurrentContent();
+    const body = JSON.stringify(convertToRaw(currentContent));
+  
     const message = {
-      body: this.state.body,
+      body,
       channelId: this.props.channelSlug,
       parentMessageId: this.props.parentMessageId
     };
-    
+
     this.props.createMessageRequest(message, this.props.parentMessageSlug);
-    this.setState({ body: '' });
   }
 
   render() {
     return (
-      <div className="message-form-pane">
+      <div className="msg-form-container">
         <FormErrors entity="message" />
-
-        <form onSubmit={this.handleMessageSubmit}>
-          <textarea
-            onChange={this.handleTextareaValue}
-            value={this.state.body}
+        
+        {this.state.htmlMessageBody}
+        
+        <form className="msg-form" onSubmit={this.handleMessageSubmit}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            plugins={[emojiPlugin]}
           />
-          <input type="submit" value="Add Message" />
+
+          <EmojiSuggestions />
+          <EmojiSelect />
+
+          <button type="submit" className="btn btn__submit">
+            Add Message
+          </button>
         </form>
       </div>
     );
