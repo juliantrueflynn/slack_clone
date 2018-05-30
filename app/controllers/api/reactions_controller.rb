5025424c5128b088e8147e3b1cad1477d06cfe1e
Julian <hello@juliantrueflynn.com>
2018-05-30 16:00:16 -0400

    Add controller for Reactions

---
+++ app/controllers/api/reactions_controller.rb
@@ -0,0 +1,35 @@
+class Api::ReactionsController < ApplicationController
+  before_action :set_reaction, only: [:show, :destroy]
+
+  def show
+  end
+
+  def create
+    @reaction = Reaction.new(reaction_params)
+
+    if @reaction.save
+      render json: @reaction
+    else
+      render json: @reaction.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    if @reaction
+      @reaction.destroy
+      render json: @reaction
+    else
+      render json: ["doesn't exist"]
+    end
+  end
+
+  private
+
+  def set_reaction
+    @reaction = Reaction.find_by(params[:id])
+  end
+
+  def reaction_params
+    params.require(:reaction).permit(:message_id, :user_id, :reaction)
+  end
+end
