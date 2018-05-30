378e379c89aab5b28ba15a598877de92c62ba5ad
Julian <hello@juliantrueflynn.com>
2018-05-30 14:57:53 -0400

    Generate migration for Reactions

---
+++ db/migrate/20180530185223_create_reactions.rb
@@ -0,0 +1,11 @@
+class CreateReactions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reactions do |t|
+      t.references :message, index: true, foreign_key: true, null: false
+      t.references :user, index: true, foreign_key: true, null: false
+      t.string :reaction, null: false
+
+      t.timestamps
+    end
+  end
+end
