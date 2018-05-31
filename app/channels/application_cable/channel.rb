0777b5f077d3d8673adcb85de39f9a393e0781cb
Julian <hello@juliantrueflynn.com>
2018-05-31 17:42:22 -0400

    Remove needless method

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,12 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    def create(data)
-      Message.create(
-        body: data.fetch('body'),
-        author_id: data.fetch('author_id'),
-        channel_id: data.fetch('channel_id'),
-        parent_message_id: data.fetch('parent_message_id')
-      )
-    end
   end
 end
