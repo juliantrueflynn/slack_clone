8b7190ac22bc8b1f266f5a7d64f87e6ca069d1d0
Julian <hello@juliantrueflynn.com>
2018-06-01 18:21:53 -0400

    Remove needless dispatcher

--- frontend/components/Workspace/WorkspacePageContainer.js
+++ frontend/components/Workspace/WorkspacePageContainer.js
@@ -2,13 +2,9 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import WorkspacePage from './WorkspacePage';
 import { workspaceRequest } from '../../actions/workspaceActions';
-import { createWorkspaceSubReceive } from '../../actions/workspaceSubActions';
 
 const mapDispatchToProps = dispatch => ({
-  workspaceRequest: workspaceSlug => dispatch(workspaceRequest(workspaceSlug)),
-  createWorkspaceSubReceive: workspaceSub => dispatch(
-    createWorkspaceSubReceive(workspaceSub)
-  )
+  workspaceRequest: workspaceSlug => dispatch(workspaceRequest(workspaceSlug))
 });
 
 export default withRouter(
