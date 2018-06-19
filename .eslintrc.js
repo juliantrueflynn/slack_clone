b5fa750130c7a614b500565cc12528fe2339dd9a
Julian <hello@juliantrueflynn.com>
2018-06-19 18:21:25 -0400

    Ignore comma dangle and Link valid href rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-        "react/prop-types": 0
+        "react/prop-types": 0,
+        "jsx-a11y/anchor-is-valid": [ "error", {
+            "components": [ "Link" ],
+            "specialLink": [ "to" ]
+        }],
+        "comma-dangle": ["error", {
+            "arrays": "ignore",
+            "objects": "ignore",
+            "imports": "ignore",
+            "exports": "ignore",
+            "functions": "ignore"
+        }]
     },
     "globals": { "fetch": false }
 };
