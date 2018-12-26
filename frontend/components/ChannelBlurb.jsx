6a99b0d59479149d3cea9a4bb19e8409a5029213
Julian <hello@juliantrueflynn.com>
2018-12-26 13:53:41 -0500

    Remove arrow functions

--- frontend/components/ChannelBlurb.jsx
+++ frontend/components/ChannelBlurb.jsx
@@ -6,84 +6,89 @@ import { dateUtil } from '../util/dateUtil';
 import Button from './Button';
 import './ChannelBlurb.css';
 
-const ChannelBlurb = ({
-  channel,
-  currentUserSlug,
-  openModal,
-  matchUrl,
-}) => {
-  const {
-    hasDm,
-    dmUserSlug,
-    ownerSlug,
-    ownerName,
-    createdAt,
-    topic,
-    title,
-  } = channel;
-  const openEditor = () => openModal('MODAL_FORM_CHANNEL', { channel, currentUserSlug });
-  const userPathUrl = userSlug => `${matchUrl}/team/${userSlug}`;
-  let chatTitle = title;
-  let description;
-
-  if (hasDm) {
-    description = (
-      <div className="ChannelBlurb__description">
-        {'This is the beginning of your direct message conversation with '}
-        <Link className="ChannelBlurb__label" to={userPathUrl(dmUserSlug)}>{title}</Link>
-      </div>
-    );
-  } else {
-    let creator = (
-      <Link className="ChannelBlurb__label" to={userPathUrl(ownerSlug)}>{ownerName}</Link>
-    );
-
-    if (currentUserSlug === ownerSlug) {
-      creator = 'You';
-    }
-
-    const date = dateUtil(createdAt);
-    const dateCreated = date.isToday() ? 'today' : `on ${date.monthName()} ${date.dayOrdinal()}`;
-
-    chatTitle = <span className="ChannelBlurb__title-text">{title}</span>;
-
-    description = (
-      <div className="ChannelBlurb__description">
-        {creator}
-        {` created this channel ${dateCreated}. This is the beginning of the `}
-        {chatTitle}
-        {' channel. '}
-        {topic && `Purpose: ${topic} `}
-        {topic && (
-          <span className="ChannelBlurb__parentheses">
-            <Button buttonFor="edit" onClick={openEditor} unStyled>
-              edit
-            </Button>
-          </span>
-        )}
-      </div>
-    );
+class ChannelBlurb extends React.Component {
+  constructor(props) {
+    super(props);
+    this.openEditor = this.openEditor.bind(this);
   }
 
-  const blurbClassNames = classNames('ChannelBlurb', {
-    ChannelBlurb__dm: hasDm,
-    ChannelBlurb__channel: !hasDm,
-  });
+  userPathUrl(userSlug) {
+    const { matchUrl } = this.props;
+    return `${matchUrl}/team/${userSlug}`;
+  }
 
-  return (
-    <section className={blurbClassNames}>
-      <h2 className="ChannelBlurb__title">{chatTitle}</h2>
-      {description}
-      {!topic && !hasDm && (
-        <div className="ChannelBlurb__editor">
-          <Button buttonFor="edit-label" onClick={openEditor} unStyled>
-            <FontAwesomeIcon icon="pencil-alt" size="xs" />
-            Set a topic
-          </Button>
+  openEditor() {
+    const { openModal, channel, currentUserSlug } = this.props;
+
+    openModal('MODAL_FORM_CHANNEL', { channel, currentUserSlug });
+  }
+
+  render() {
+    const { channel, currentUserSlug } = this.props;
+    const {
+      hasDm,
+      dmUserSlug,
+      ownerSlug,
+      ownerName,
+      createdAt,
+      topic,
+      title,
+    } = channel;
+    let description = (
+      <div className="ChannelBlurb__description">
+        {'This is the beginning of your direct message conversation with '}
+        <Link className="ChannelBlurb__label" to={this.userPathUrl(dmUserSlug)}>{title}</Link>
+      </div>
+    );
+
+    if (!hasDm) {
+      let creator = (
+        <Link className="ChannelBlurb__label" to={this.userPathUrl(ownerSlug)}>{ownerName}</Link>
+      );
+
+      if (currentUserSlug === ownerSlug) {
+        creator = 'You';
+      }
+
+      const date = dateUtil(createdAt);
+      const dateCreated = date.isToday() ? 'today' : `on ${date.monthName()} ${date.dayOrdinal()}`;
+
+      description = (
+        <div className="ChannelBlurb__description">
+          {creator}
+          {` created this channel ${dateCreated}. This is the beginning of the `}
+          {title}
+          {' channel. '}
+          {topic && `Purpose: ${topic} `}
+          {topic && (
+            <span className="ChannelBlurb__parentheses">
+              <Button buttonFor="edit" onClick={this.openEditor} unStyled>edit</Button>
+            </span>
+          )}
         </div>
-      )}
-    </section>
-  );
-};
+      );
+    }
+
+    const blurbClassNames = classNames('ChannelBlurb', {
+      ChannelBlurb__dm: hasDm,
+      ChannelBlurb__channel: !hasDm,
+    });
+
+    return (
+      <section className={blurbClassNames}>
+        <h2 className="ChannelBlurb__title">{title}</h2>
+        {description}
+        {!topic && !hasDm && (
+          <div className="ChannelBlurb__editor">
+            <Button buttonFor="edit-label" onClick={this.openEditor} unStyled>
+              <FontAwesomeIcon icon="pencil-alt" size="xs" />
+              Set a topic
+            </Button>
+          </div>
+        )}
+      </section>
+    );
+  }
+}
 
 export default ChannelBlurb;
