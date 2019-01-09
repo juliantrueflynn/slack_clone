c540e717559ec5fbfc7f4555acfcd31974feeeae
Julian <hello@juliantrueflynn.com>
2019-01-09 18:08:29 -0500

    Separate MessageThreadDrawer styles

--- frontend/components/MessageThreadDrawer.jsx
+++ frontend/components/MessageThreadDrawer.jsx
@@ -3,6 +3,7 @@ import MessagesListContainer from './MessagesListContainer';
 import MessageForm from './MessageForm';
 import ScrollBar from './ScrollBar';
 import withScrollManager from './withScrollManager';
+import './MessageThreadDrawer.css';
 
 const MessageThreadDrawer = ({ messages, createMessageRequest, ...props }) => {
   const parentMessage = messages[0];
@@ -13,22 +14,24 @@ const MessageThreadDrawer = ({ messages, createMessageRequest, ...props }) => {
   }
 
   return (
-    <ScrollBar {...props}>
-      <MessagesListContainer
-        messages={messages}
-        role="listitem"
-        filterMenuItems={filterMenuItems}
-        isThreadHidden
-        isHoverable
-      />
-      <MessageForm
-        channelId={parentMessage.channelId}
-        parentMessageId={parentMessage.id}
-        parentMessageSlug={parentMessage.slug}
-        createMessageRequest={createMessageRequest}
-        hasSubmitButton
-      />
-    </ScrollBar>
+    <div className="MessageThreadDrawer">
+      <ScrollBar {...props}>
+        <MessagesListContainer
+          messages={messages}
+          role="listitem"
+          filterMenuItems={filterMenuItems}
+          isThreadHidden
+          isHoverable
+        />
+        <MessageForm
+          channelId={parentMessage.channelId}
+          parentMessageId={parentMessage.id}
+          parentMessageSlug={parentMessage.slug}
+          createMessageRequest={createMessageRequest}
+          hasSubmitButton
+        />
+      </ScrollBar>
+    </div>
   );
 };
 
