229fd618838e099c383c41a4bddcfafeba509544
Julian <hello@juliantrueflynn.com>
2019-01-11 00:19:18 -0500

    Fix find_or_initialize_by missing method and remove updating created_at attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ def random_num(min: 3, max: 9)
   [*min.to_i..max.to_i].sample
 end
 
-def random_string
+def generate_message_body
   lorem_ipsum = [
     Faker::Lorem.paragraph(random_num, rand < 0.5, random_num),
     Faker::Lorem.paragraph(random_num),
@@ -23,7 +23,7 @@ def random_message_body
   '{"blocks":[{"key":"' +
   SecureRandom.urlsafe_base64(6) +
   '","text":"' +
-  random_string +
+  generate_message_body +
   '","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}'
 end
 
@@ -45,18 +45,12 @@ def seed_chats(user)
   workspace = user.workspaces.sample
   return unless workspace
 
-  created_at = DateTime.now - 6
   title = Faker::Company.unique.buzzword
-  user.created_channels.create(
-    title: title,
-    workspace_id: workspace.id,
-    created_at: created_at,
-    updated_at: created_at
-  )
+  user.created_channels.create(title: title, workspace_id: workspace.id)
 end
 
-def update_channel_reads(channel)
-  read = channel.reads.find_or_initialize(user_id: 1)
+def update_first_user_entity_reads(entity)
+  read = entity.reads.find_or_initialize_by(user_id: 1)
   read.save!
 end
 
@@ -72,39 +66,19 @@ User.create!(email: "jtf@gmail.com", username: "jtf", password: "123456")
 
 3.times do
   title = Faker::Company.unique.name
-  created_at = DateTime.now - 8
-
   workspace = User.first.created_workspaces.create(
     title: title,
     slug: title.parameterize,
-    created_at: created_at,
-    updated_at: created_at,
-    skip_broadcast: true
   )
-
-  default_channels = %w(general random).reduce([]) do |memo, title|
-    memo << {
-      title: title,
-      workspace_id: workspace.id,
-      created_at: created_at,
-      updated_at: created_at
-    }
-  end
-
-  User.first.created_channels.create(default_channels)
-  
-  workspace.channels.first(2).each { |ch| update_channel_reads(ch) }
 
   3.times do
     chat = User.first.created_channels.create(
       title: Faker::Company.unique.buzzword,
       topic: (rand < 0.2 ? Faker::Company.bs : nil),
       workspace_id: workspace.id,
-      created_at: created_at,
-      updated_at: created_at
     )
 
-    update_channel_reads(chat)
+    update_first_user_entity_reads(chat)
   end
 end
 
@@ -122,26 +96,11 @@ end
   seed_chats(user) if rand < 0.3
 end
 
-Workspace.all.each do |workspace|
-  workspace.channels.first(2).each do |chat|
-    created_at = DateTime.now - 12
-
-    chat.update_attributes(
-      created_at: created_at,
-      updated_at: created_at
-    )
-  end
-end
-
 User.first.workspaces.each do |workspace|
   chats = User.first.channels.where(workspace_id: workspace.id)
+
   chats.each do |chat|
-    chat.messages.create(
-      body: random_message_body,
-      author_id: 1,
-      created_at: chat.created_at + 1,
-      updated_at: chat.created_at + 1
-    )
+    chat.messages.create(body: random_message_body, author_id: 1)
   end
 end
 
@@ -182,23 +141,9 @@ end
   generate_message_interaction(chat) if rand < 0.7
 end
 
-Channel.all.each do |chat|
-  sub_messages = chat.messages.take_while do |sub_msg|
-    sub_msg.update_attributes(
-      created_at: chat.created_at,
-      updated_at: chat.created_at
-    )
-  end
-end
-
-Workspace.all.each do |workspace|
-  all_convos = workspace.messages.convos_with_author_id(1).with_parent
-
-  reads = all_convos.reduce([]) do |memo, message|
-    memo << { readable_id: message.id, readable_type: 'Message' }
-  end
-
-  User.first.reads.create(reads)
+User.first.workspaces.each do |workspace|
+  all_convos = workspace.messages.convo_parents_with_author_id(1)
+  all_convos.each { |convo| update_first_user_entity_reads(convo) }
 end
 
 20.times do
