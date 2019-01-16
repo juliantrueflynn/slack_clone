4b6e06a7ca8fafaf42d35a7ee082a74936b9d2c7
Julian <hello@juliantrueflynn.com>
2019-01-15 23:03:11 -0500

    Add message for empty favorites drawer

--- frontend/components/FavoritesDrawer.jsx
+++ frontend/components/FavoritesDrawer.jsx
@@ -5,6 +5,14 @@ import './FavoritesDrawer.css';
 const FavoritesDrawer = ({ messages }) => {
   const filterMenuItems = ['dropdown', 'reaction', 'convo'];
 
+  if (!messages.length) {
+    return (
+      <div className="FavoritesDrawer FavoritesDrawer--empty">
+        You haven&#8217;t favorited anything yet!
+      </div>
+    );
+  }
+
   return (
     <div className="FavoritesDrawer">
       <MessagesListContainer
